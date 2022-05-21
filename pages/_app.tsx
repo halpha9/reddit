bb4fa56819f9483abceb2bf8dd4d815fab3f0405
halpha9 <harryalpha@hotmail.co.uk>
2022-05-21 18:02:53 +0100

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,12 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <SessionProvider session={session}>
+  <Component {...pageProps} />
+  </SessionProvider>
+  )
+
 }
 
 export default MyApp
